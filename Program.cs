1bf1b4284f2fe228b65521e02417e72291b62858
FrknKoseoglu <furkan1771@gmail.com>
2017-11-28 00:33:18 +0300

    Project Euler Problem 3 solution in C# Console
    Project Euler Problem 3 solution in C# Console

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ProjectEuler_Problem_3
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            long s2;
+            s2 = 600851475143;
+            System.Console.Write("-----Doğruluk Kontrol-----");
+            System.Console.WriteLine();
+            for (int i = 2; i < s2; ++i)
+            {
+                if (s2 % i == 0)
+                {
+                    System.Console.Write(i + " / " + s2 + " = ");
+                    s2 = s2 / i;
+                    System.Console.Write(s2);
+                    System.Console.WriteLine();
+                    i = 2;
+                }
+            }
+
+            System.Console.Write("-----Cevap-----");
+            System.Console.WriteLine();
+            System.Console.Write(s2);
+            System.Console.WriteLine();
+        }
+    }
+}
